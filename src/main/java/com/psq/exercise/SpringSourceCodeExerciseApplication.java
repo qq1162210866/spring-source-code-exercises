a6e799fbfea4304757ec56b5c2031717a29c32ea
pengshiquan <1162210866@qq.com>
2022-07-27 17:34:01 +0800

    源码项目初始化提交
    源码项目初始化提交

---
+++ src/main/java/com/psq/exercise/SpringSourceCodeExerciseApplication.java
@@ -0,0 +1,9 @@
+package com.psq.exercise;
+
+public class SpringSourceCodeExerciseApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/com/psq/exercise/SpringSourceCodeExerciseApplicationTests.java
@@ -0,0 +1,9 @@
+package com.psq.exercise;
+
+public class SpringSourceCodeExerciseApplicationTests {
+
+
+    public void contextLoads() {
+    }
+
+}
